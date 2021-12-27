006eb7d1d3b9e4ea327372a08ca8c0da3739f08c
willyanfx <willyan.fx@gmail.com>
2021-12-27 11:01:05 -0300

    stitches css-n-jsx

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,5 +1,47 @@
+// Path: src/buttons/index.tsx
 import React from 'react';
+import { styled } from '@stitches/react';
 
-export const Button = () => {
-  return <button>Hello world</button>;
-};
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant?: 'secondary' | 'primary' | 'danger';
+  size?: 'small' | 'medium' | 'large';
+  isFullWidth?: boolean;
+}
+
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<'button'>
+>(({ children, variant, size, isFullWidth, ...props }: ButtonProps, ref) => {
+  return (
+    <UIButton
+      ref={ref}
+      variant={variant}
+      size={size}
+      isFullWidth={isFullWidth}
+      type="button"
+      {...props}
+    >
+      {children}
+    </UIButton>
+  );
+});
+
+const UIButton = styled('button', {
+  backgroundColor: 'gainsboro',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray'
+  }
+});
+
+Button.defaultProps = {
+  variant: 'primary',
+  size: 'medium',
+  isFullWidth: false
+} as ButtonProps;
+
+Button.displayName = 'Button';
+
+export { Button };
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -19,23 +19,11 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
+  variant: 'primary',
   label: 'Button'
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button'
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button'
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button'
+  children: 'Secondary'
 };
